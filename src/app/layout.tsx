c22fb64ba1d04a76fd846005b8798354ce1d2b34
Alexandre <aleeh.rodrigues92@gmail.com>
2025-11-07 20:54:44 -0300

    fix: adicionando e atualizando metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-// Configurando a fonte Inter com variável CSS
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -11,6 +10,28 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Alexandre Rodrigues | Desenvolvedor Full Stack",
   description: "Portfólio e projetos de Alexandre Rodrigues, desenvolvedor Full Stack Júnior.",
+  authors: [{ name: "Alexandre Rodrigues" }],
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://portfolio-1qbf9g2ia-alexandre-rodrigues-s-projects.vercel.app/",
+    title: "Alexandre Rodrigues | Desenvolvedor Full Stack",
+    description: "Software Developer | HTML | CSS | Tailwind | JavaScript | React | Next.js | Python",
+    siteName: "Alexandre Rodrigues Portfolio",
+    images: [
+      {
+        url: "/assets/images/portfolioScreenshot.jpg", 
+        height: 630,
+        alt: "Alexandre Rodrigues - Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Alexandre Rodrigues | Desenvolvedor Full Stack",
+    description: "Software Developer | HTML | CSS | Tailwind | JavaScript | React | Next.js | Python",
+    images: ["/assets/images/perfilPortfolio.png"],
+  },
 };
 
 export default function RootLayout({
